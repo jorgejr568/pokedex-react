016d7f263a32603b781dfa4f0c81cb954785d9fa
Jorge Junior <jorge.junior.568@live.com>
2021-01-22 08:53:41 -0300

    eslint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { HomePage } from './components/pages'
 
 const App = () => {
